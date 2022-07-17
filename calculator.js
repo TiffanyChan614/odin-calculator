4a1656bee7766d80e0c02bdadb4ad421f5beeb9e
TiffChan <tiffanychan1999614@gmail.com>
2022-07-17 22:22:26 +0800

    Enable digits to be show in display

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+const display = document.querySelector(".display");
+const digitsBtns = document.querySelectorAll(".digits");
+var displayValue = "";
+
 function add(n1, n2){
     return n1 + n2;
 }
@@ -17,3 +21,15 @@ function divide(n1, n2){
 function operate(operator, n1, n2){
     return operator(n1, n2);
 }
+
+function fillDisplay(){
+    Array.from(digitsBtns).forEach(button =>
+        button.addEventListener('click', () => {
+            const num = button.textContent;
+            displayValue += num;
+            display.textContent += num;
+            //console.log(displayValue);
+        }));
+}
+
+fillDisplay()
