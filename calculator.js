b03bd73685979cd50786b2957d63c9eac54c09df
TiffChan <tiffanychan1999614@gmail.com>
2022-07-17 23:39:29 +0800

    Add calculate function to perform math operations on two inputs

--- calculator.js
+++ calculator.js
@@ -1,35 +1,58 @@
 const display = document.querySelector(".display");
 const digitsBtns = document.querySelectorAll(".digits");
-var displayValue = "";
+const operBtns = document.querySelectorAll(".operators");
+const equalBtn = document.querySelector(".equal");
 
-function add(n1, n2){
+var operators = {
+    "Add": function(n1, n2){
     return n1 + n2;
-}
+    },
 
-function subtract(n1, n2){
+    "Subtract": function(n1, n2){
     return n1 - n2;
-}
+    },
 
-function multiply(n1, n2){
+    "Multiply": function(n1, n2){
     return n1 * n2;
-}
+    },
 
-function divide(n1, n2){
+    "Divide": function(n1, n2){
     return n1 / n2;
-}
+    }
+};
 
 function operate(operator, n1, n2){
     return operator(n1, n2);
 }
 
 function fillDisplay(){
-    Array.from(digitsBtns).forEach(button =>
+    Array.from(digitsBtns).forEach((button) =>
         button.addEventListener('click', () => {
             const num = button.textContent;
-            displayValue += num;
+            console.log(num);
             display.textContent += num;
-            //console.log(displayValue);
         }));
 }
 
-fillDisplay()
+function calculate(){
+    let firstNum, secondNum;
+    let oper;
+
+    fillDisplay();
+
+    Array.from(operBtns).forEach((button) =>
+        button.addEventListener('click', () => {
+            oper = button.textContent;
+            firstNum = parseInt(display.textContent);
+            display.textContent = "";
+        })
+    );
+
+    equalBtn.addEventListener('click', () => {
+        secondNum = parseInt(display.textContent);
+        let res = operate(operators[oper], firstNum, secondNum);
+        display.textContent = res;
+    })
+}
+
+calculate();
