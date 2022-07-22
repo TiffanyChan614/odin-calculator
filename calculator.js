e50164139aeed6f82875ee17654d10e6bc9beb58
TiffChan <tiffanychan1999614@gmail.com>
2022-07-23 01:36:54 +0800

    Make buttons change color when corresponding key is pressed

--- calculator.js
+++ calculator.js
@@ -1,4 +1,5 @@
 const displayVar = document.querySelector(".display-content");
+const buttons = document.querySelector("button");
 const digitsBtns = document.querySelectorAll(".digits");
 const operBtns = document.querySelectorAll(".operators");
 const equalBtn = document.querySelector(".equal");
@@ -12,41 +13,63 @@ const equalKey = "Enter";
 const bsKey = "Backspace";
 const acKey = "Escape";
 
-var operators = {
-    "+": function(n1, n2){
+const keyButton = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    [`${dotKey}`]: "dot",
+    "+": "plus",
+    "-": "minus",
+    "*": "multiply",
+    "/": "divide",
+    "%": "modulo",
+    [`${equalKey}`]: "equal",
+    [`${bsKey}`]: "bs",
+    [`${acKey}`]: "clear",
+}
+
+const operators = {
+    "+": function(n1, n2) {
         return n1 + n2;
     },
 
-    "-": function(n1, n2){
+    "-": function(n1, n2) {
         return n1 - n2;
     },
 
-    "*": function(n1, n2){
+    "*": function(n1, n2) {
         return n1 * n2;
     },
 
-    "/": function(n1, n2){
+    "/": function(n1, n2) {
         if (n2 === 0) return "ERROR";
         return n1 / n2;
     },
 
-    "%": function(n1, n2){
+    "%": function(n1, n2) {
         return n1 % n2;
     }
 };
 
-function operate(operator, n1, n2){
+function operate(operator, n1, n2) {
     return operator(n1, n2);
 }
 
-function calculate(){
+function calculate() {
     let firstNum = null;
     let secondNum = null;
     let oper = null;
     let resShown = false;
     let dotPressed = false;
 
-    function enterDigit(num){
+    function enterDigit(num) {
         if (resShown) {
             displayVar.textContent = "";
             resShown = false;
@@ -56,8 +79,7 @@ function calculate(){
 
     function enterOper(op){
         let tempNum = parseFloat(displayVar.textContent);
-        // console.log("tempNum", tempNum);
-        if (!isNaN(tempNum)){
+        if (!isNaN(tempNum)) {
             if (firstNum !== null) {
                 secondNum = tempNum;
                 firstNum = operate(operators[oper], firstNum, secondNum);
@@ -69,21 +91,19 @@ function calculate(){
             oper = op;
             displayVar.textContent = "";
             dotPressed = false;
-            // console.log("firstNum", firstNum);
-            // console.log("secondNum", secondNum);
         }
-        else if(oper) oper = op;
+        else if (oper) oper = op;
     }
 
-    function enterEqual(){
-        if (displayVar.textContent){
+    function enterEqual() {
+        if (displayVar.textContent) {
             secondNum = parseFloat(displayVar.textContent);
             dotPressed = false;
         }
 
         if (firstNum !== null &&
                 secondNum !== null &&
-                oper !== null){
+                oper !== null) {
             let res = operate(operators[oper], firstNum, secondNum);
             if (typeof res === "number"){
                 if (res.toString().length > 11) {
@@ -91,8 +111,9 @@ function calculate(){
                 }
                 else displayVar.textContent = res.toString();
             }
-            else displayVar.textContent = res;
-            // console.log("res", res);
+            else {
+                displayVar.textContent = res;
+            }
             firstNum = null;
             secondNum = null;
             oper = null;
@@ -100,7 +121,7 @@ function calculate(){
         }
     }
 
-    function enterClear(){
+    function enterClear() {
         displayVar.textContent = "";
         firstNum = null;
         secondNum = null;
@@ -108,21 +129,21 @@ function calculate(){
         resShown = false;
     }
 
-    function enterDot(){
+    function enterDot() {
         if (!dotPressed){
             displayVar.textContent += ".";
             dotPressed = true;
         }
     }
 
-    function enterBs(){
+    function enterBs() {
         let oldContent = displayVar.textContent;
         displayVar.textContent = oldContent.slice(0, oldContent.length-1);
     }
 
     Array.from(digitsBtns).forEach((button) =>
     button.addEventListener('click', () => {
-        const num = button.textContent;
+        let num = button.textContent;
         enterDigit(num);
     }));
 
@@ -142,11 +163,18 @@ function calculate(){
 
     document.addEventListener('keydown', (e) => {
         if (numsKey.includes(e.key)) enterDigit(e.key);
-        else if(opersKey.includes(e.key)) enterOper(e.key);
-        else if(e.key === equalKey) enterEqual();
-        else if(e.key === acKey) enterClear();
-        else if(e.key === dotKey) enterDot();
-        else if(e.key === bsKey) enterBs();
+        else if (opersKey.includes(e.key)) enterOper(e.key);
+        else if (e.key === equalKey) enterEqual();
+        else if (e.key === acKey) enterClear();
+        else if (e.key === dotKey) enterDot();
+        else if (e.key === bsKey) enterBs();
+        let targetBtn = document.querySelector(`.${keyButton[e.key]}`);
+        if (targetBtn !== null) targetBtn.classList.add("pressed");
+    });
+
+    document.addEventListener('keyup', (e) => {
+        let targetBtn = document.querySelector(`.${keyButton[e.key]}`);
+        if(targetBtn !== null) targetBtn.classList.remove("pressed");
     });
 }
 
